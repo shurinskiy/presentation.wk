6e9076c6473834dc54071078a91b3ab9a32ad7a4
Alexandr Tumanov <shurinskiy2@gmail.com>
2025-06-07 05:56:00 +0500

    add form

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,9 +1,39 @@
+import { disablePageScroll, enablePageScroll } from '@fluejs/noscroll';
+import { driveMenu } from "../../js/libs/driveMenu";
+
 (() => {
 	const header = document.querySelector('[data-header-js]');
 	if (! header) return;
 
+	const navi = header.querySelector('.header__menu');
+	const toggles = header.querySelectorAll('.header__toggle, .header__close');
+	
+	const menu = driveMenu(navi, toggles, {
+		omitToClose: '.modal',
+		class: 'opened',
+		open: function() {
+			disablePageScroll();
+			document.body.classList.add('underlay');
+
+			this.querySelectorAll('a.header__link[href*="#"]').forEach(link => {
+				link.addEventListener('click', (e) => menu.menuClose(e));
+			});
+		},
+		close: function() {
+			enablePageScroll();
+			document.body.classList.add('underlay_closing');
+			
+			this.addEventListener('transitionend', e => {
+				document.body.classList.remove('underlay', 'underlay_closing');
+			}, { once: true });
+		}
+	});
+
+
 	window.addEventListener('scroll', () => {
 		header.classList[window.scrollY < 30 ? 'remove': 'add']('header_scrolled');
 	});
+
+	navi.addEventListener('swiped-left', (e) => menu.menuClose(e));
 
 })();
--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -2,16 +2,52 @@ import { disablePageScroll, enablePageScroll } from '@fluejs/noscroll';
 import { makeModal } from "../../js/libs/makeModal";
 
 (() => {
+	const selector = [...document.querySelectorAll('.portfolio__item[data-modal]')]
+		.map(item => item.dataset.modal)
+		.join(', ');
+
+	if (! selector) return;
+
+	const items = document.querySelectorAll(selector);
+	let currentIdx = 0;
+
+	const movePrev = () => {
+		if (currentIdx > 0) {
+			currentIdx--;
+			modal.open(items[currentIdx]);
+		}
+	};
+
+	const moveNext = () => {
+		if (currentIdx < items.length - 1) {
+			currentIdx++;
+			modal.open(items[currentIdx]);
+		}
+	};
+
 	const modal = makeModal({
 		preserve: true,
 		init(underlay) {
 
 		},
-		open(modal, button) {
+		open(modal, source) {
+			enablePageScroll();
 			disablePageScroll();
+			
+			modal.buttons();
+
+			if (source.dataset.modal) {
+				currentIdx = [...items].findIndex(item => item.id.includes(source.dataset.modal.slice(1)));
+			}
+
+			modal.prev.addEventListener('click', movePrev);
+			modal.next.addEventListener('click', moveNext);
+			this.addEventListener('swiped-right', movePrev);
+			this.addEventListener('swiped-left', moveNext);
 		},
 		close() {
 			enablePageScroll();
+			currentIdx = 0;
 		}
 	});
 
